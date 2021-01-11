a553df9f408001eb89c8c77741e5bcf179669269
hanquan lian <1051576073@qq.com>
2021-01-11 22:26:48 +0800

    [add]添加 队列

---
+++ Sim/include/TaskPool.hpp
@@ -0,0 +1,19 @@
+/*
+* 任务池，动态拓展
+*/
+#ifndef SIM_TASK_POOL_HPP_
+#define SIM_TASK_POOL_HPP_
+namespace sim
+{
+	class TaskPool
+	{
+	public:
+		TaskPool();
+		~TaskPool();
+
+	private:
+
+	};
+
+}
+#endif
