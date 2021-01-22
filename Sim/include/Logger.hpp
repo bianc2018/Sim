f6d80cc7e81403fba2e497a30367ac8796bbda5a
hanquan lian <1051576073@qq.com>
2021-01-22 23:35:00 +0800

    [modify]1.日志加入格式化宏，2.错误日志输出的时候显示为红色

--- Sim/include/Logger.hpp
+++ Sim/include/Logger.hpp
@@ -5,9 +5,47 @@
 #define SIM_LOGGER_HPP_
 #include <stdio.h>
 #include <sstream>
+#include <iomanip>
+
+//平台相关
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+	#ifndef OS_WINDOWS
+		#define OS_WINDOWS
+	#endif
+	#include <Windows.h>
+
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+	#ifndef OS_LINUX
+		#define OS_LINUX
+	#endif 
+	#define NONE         "/033[m"
+	#define RED          "/033[0;32;31m"
+	#define LIGHT_RED    "/033[1;31m"
+	#define GREEN        "/033[0;32;32m"
+	#define LIGHT_GREEN  "/033[1;32m"
+	#define BLUE         "/033[0;32;34m"
+	#define LIGHT_BLUE   "/033[1;34m"
+	#define DARY_GRAY    "/033[1;30m"
+	#define CYAN         "/033[0;36m"
+	#define LIGHT_CYAN   "/033[1;36m"
+	#define PURPLE       "/033[0;35m"
+	#define LIGHT_PURPLE "/033[1;35m"
+	#define BROWN        "/033[0;33m"
+	#define YELLOW       "/033[1;33m"
+	#define LIGHT_GRAY   "/033[0;37m"
+	#define WHITE        "/033[1;37m"
+#else
+	#error "不支持的平台"
+#endif
 
 //日志宏
 #ifndef SIM_NO_LOGGER
+	//格式
+	#define SIM_FORMAT_NUM(num,base,w,f)	std::setbase(base) << std::setw(w) << std::setfill(f) << num
+	#define SIM_FORMAT_STR(str,w,f)			std::setw(2) << std::setfill(f) << str
+	#define SIM_HEX(num) SIM_FORMAT_NUM(num,16,8,'0');
+	#define SIM_FORMAT_STR0(str,w) SIM_FORMAT_STR(str,w,' ')
+
 	//防止重名
 	#define SIM_FUNC(lv)\
 		sim::LoggerFunction LoggerFunction1263637187321356273517631263(lv,__FUNCTION__,__LINE__)
@@ -29,6 +67,11 @@
 	#define SIM_LWARN(x) SIM_LOG(sim::LWarn,x)
 	#define SIM_LERROR(x) SIM_LOG(sim::LError,x)
 #else
+	#define SIM_FORMAT_NUM(num,base,w,f)	
+	#define SIM_FORMAT_STR(str,w,f)			
+	#define SIM_HEX(num) 
+	#define SIM_FORMAT_STR0(str,w) 
+
 	#define SIM_FUNC(lv)
 	#define SIM_FUNC_DEBUG() SIM_FUNC(sim::LDebug)
 	#define SIM_LOG_CONFIG(max_lv,handler,userdata)
@@ -91,6 +134,9 @@ namespace sim
 		LoggerHandler handler_;
 		//用户数据
 		void* userdata_;
+#ifdef OS_WINDOWS
+		HANDLE std_handle_;
+#endif
 	};
 
 	//函数接口输入
@@ -109,7 +155,9 @@ namespace sim
 		:max_level_(LError)
 		, handler_(NULL)
 		, userdata_(NULL)
+		, std_handle_ (NULL)
 	{
+		std_handle_ = GetStdHandle(STD_OUTPUT_HANDLE);
 	}
 
 	Logger& sim::Logger::GetLog()
@@ -151,8 +199,27 @@ namespace sim
 	{
 		static char LvInfo[5][8] = { "None","Error","Warning","Info","Debug"};
 		//printf("%-8s %-30s %4u:%s\n", LvInfo[lv], func, line, msg);
-		printf("%s.%s.%u: %s\n", LvInfo[lv], func, line, msg);
-		//printf("%s\n", msg);
+#ifdef OS_WINDOWS
+		if (lv == LError)
+		{
+			SetConsoleTextAttribute(std_handle_, FOREGROUND_INTENSITY | FOREGROUND_RED);
+			printf("%s.%s.%u: %s\n", LvInfo[lv], func, line, msg);
+			SetConsoleTextAttribute(std_handle_,
+				FOREGROUND_RED |
+				FOREGROUND_GREEN |
+				FOREGROUND_BLUE);
+		}
+		else
+		{
+			printf("%s.%s.%u: %s\n", LvInfo[lv], func, line, msg);
+		}
+#else
+		///033[1;31;40m 输出红色字符 /033[0m
+		if (lv == LError)
+			printf("/033[1;31;40m  %s.%s.%u: %s /033[0m\n", LvInfo[lv], func, line, msg);
+		else
+			printf("%s.%s.%u: %s\n", LvInfo[lv], func, line, msg);
+#endif
 	}
 
 	sim::LoggerFunction::LoggerFunction(LogLevel lv, 
